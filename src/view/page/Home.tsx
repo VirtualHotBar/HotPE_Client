608f6f943714d6d3fe0302e0e87849f833bb3c39
VirtualHotBar <yyds_vhb@163.com>
2023-06-29 20:55:41 +0800

    homePage

---
+++ src/view/page/Home.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+
+export default function Home(){
+
+        return(
+            <>
+            😊
+            
+            </>
+        )
+        
+
+    
+};
--- src/view/page/page.tsx
+++ src/view/page/page.tsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
+import Home from './Home';
 
-export default function Page(props:any){
-    return(
-        <>
-        {props.navKey}
-        </>
-    )
-}
+
+export default function Page(props: any) {
+    if (props.navKey == 'Home') {
+        return (<Home></Home>)
+    } else {
+
+    }
+
+};
